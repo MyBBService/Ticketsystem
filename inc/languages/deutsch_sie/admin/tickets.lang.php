0a3b79961e51878cf1ba02d35d1e877ffc1a2be0
JN-Jones <jones_h1@web.de>
2014-01-02 14:06:11 +0100

    deutsch_sie hinzugefügt

---
+++ inc/languages/deutsch_sie/admin/tickets.lang.php
@@ -0,0 +1,6 @@
+<?php
+$l['setting_group_tickets'] = "Einstellung für das Ticketsystem";
+$l['setting_group_tickets_desc'] = "Hier lassen sich die Zugriffsrechte für das Ticketsystem einstellen";
+$l['setting_tickets_usergroups'] = "Welche Nutzergruppen dürfen auf Tickets antworten?";
+$l['setting_tickets_usergroups_desc'] = "Durch Kommata trennen. Alle Mitglieder dieser Gruppen können Tickets beantworten und diese schließen. Default: \"3,4\" (Administratoren und Super Moderatoren)";
+?>
